632e385f762cf808fa008d185c08f3e0b1a66d67
clxrityy <97744702+clxrityy@users.noreply.github.com>
2026-06-14 22:13:26 -0700

    (fix) tests

--- tests/test_process.py
+++ tests/test_process.py
@@ -5,9 +5,3 @@ def test_is_safe_url():
     assert is_safe_url("http://example.com") is True
     assert is_safe_url("ftp://example.com") is False
     assert is_safe_url("javascript:alert('XSS')") is False
-
-def test_process_url():
-    assert process_url("https://example.com") == "https://example.com"
-    assert process_url("http://example.com") == "http://example.com"
-    assert process_url("https://httpbin.org/get") == "https://httpbin.org/get"
-    assert process_url("https://httpbin.org/html") == "https://httpbin.org/html"
